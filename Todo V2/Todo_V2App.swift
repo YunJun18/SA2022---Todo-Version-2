385216984369c4d2cc0543046866b873312751d4
Tan Yun Jun <yunjun09@rafflesgirlssch.edu.sg>
2022-07-02 09:11:33 +0800

    Initial Commit

---
+++ Todo V2/Todo_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_V2App.swift
+//  Todo V2
+//
+//  Created by T Krobot on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
