879b21d78dda35543556e31696cd998bfdd6b97c
Tan Yun Jun <yunjun09@rafflesgirlssch.edu.sg>
2022-07-02 09:36:52 +0800

    Created lists, navigation views, etc. Basically getting checklist layout

--- Todo V2/ContentView.swift
+++ Todo V2/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    var todos = [
+    Todo(title: "Work on skill issues"),
+    Todo(title: "Paw patrol movies"),
+    Todo(title: "Get hot chocolate", isCompleted: true),
+    Todo(title: "win more points")
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(todos) { todo in
+                HStack {
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                    Text(todo.title)
+                }
+            }
+            .navigationTitle("Skill Issues")
+        }
     }
 }
 
---
+++ Todo V2/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  Todo V2
+//
+//  Created by T Krobot on 2/7/22.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    var id = UUID()
+    
+    var title : String
+    var isCompleted = false
+}
